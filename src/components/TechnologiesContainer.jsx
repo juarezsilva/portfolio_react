b7fbd95e9dc67899ee217731d37b1836a60c6a81
Juarez Santos <juarez_santos2912@yahoo.com.br>
2023-07-30 18:11:13 -0300

    Atualizando cards

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -36,7 +36,7 @@ const TechnologiesContainer = () => {
             {tech.icon}
             <div className="technologies-info">
               <h3>{tech.name}</h3>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <p>Segue algumas tecnologias utilizadas na web.</p>
             </div>
           </div>
         ))}
